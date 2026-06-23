f390413cd54a2f5b1eaf12dd9c2c4e3997c37dee
Lebriag <paulistanotecnico@gmail.com>
2026-06-22 22:21:27 -0300

    Add files via upload

---
+++ Desafio diário 2 + Bonus - Verificador de Pares existentes.py
@@ -0,0 +1,24 @@
+'''identificador = input("Digite o identificador (ou 'sair' para encerrar): ")
+Par = 0
+
+if identificador.lower() == 'sair':
+    print("Encerrando o programa.")
+    exit() 
+   
+for i in range(1, int(identificador) + 1):
+    if i % 2 == 0:
+        Par += 1
+        
+print(Par)    '''
+
+'''recebimento = input("Digite a palavra (ou 'sair' para encerrar): ")
+if recebimento.lower() == 'sair':
+    exit()
+    
+vogais = 0
+
+for Letra in recebimento:
+    if Letra.lower() in 'aeiou':
+        vogais += 1      
+        
+print(vogais)'''
